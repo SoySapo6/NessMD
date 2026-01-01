81302603bee73dce322020f83303b9397c1b1662
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2026-01-01 17:24:31 -0500

    Update sockets-web.js

--- plugins/sockets-web.js
+++ plugins/sockets-web.js
@@ -29,7 +29,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-const PORT = 5003
+const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 const COOKIE_SECRET = 'may-7h5fa8'
 
 app.use(express.urlencoded({ extended: true }))
